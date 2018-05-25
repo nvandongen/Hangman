05a3744962cf5323ca6f312196ea889d13e2677d
navandongen <nvandongen@gmail.com>
2018-05-25 20:50:02 +0200

    Added game loop and logic

--- hangman.py
+++ hangman.py
@@ -1,16 +1,50 @@
-def letter():
-    if (getLetter) in (word):
-        print ("Ja") 
+def answerLetterInWord(letter, word):
+    if (letter in word):
+        return True
     else:
-        print("Nee")
-        
-    
+        return False
+
+def displayWrongLetters(wrongLetters):
+    display = ""
+    for letter in wrongLetters:
+        display = display + letter
+    print(display)
+
+turns = 10
+
+
+
 word = input("Please enter your secret word: ")
 
 dashes = "-" * len(word)
 
-print(dashes)
+wrongLetters = []
+rightLetters = []
 
-getLetter = input("Guess a letter: ")
+gameOver = False
 
-letter()
+while not gameOver:
+    print(dashes)
+    displayWrongLetters(wrongLetters)
+    letter = input("Guess a letter: ")
+##    TODO: Alleen eerste letter van de input lezen.
+    if answerLetterInWord(letter, word):
+        print("Yes")
+        rightLetters.append(letter)
+    else:
+        if letter in wrongLetters:
+            print("Already guessed!")
+        else:
+            wrongLetters.append(letter)
+            turns = turns - 1
+    print("Turns left: ", turns)
+    if turns == 0:
+        gameOver = True
+        print("Game over!")
+        
+
+
+
+
+
+
